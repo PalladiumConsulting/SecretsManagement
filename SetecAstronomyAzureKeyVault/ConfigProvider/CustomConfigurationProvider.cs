03d1484af805ae6239a6f4640362a910e784ff39
bechbd <dave@bechberger.com>
2015-08-09 16:18:27 -0500

    Initial Version
    This is the initial checkin of the Secrets Management blog post code
    for Windows Azure using the Azure Key Vault for secret management.

---
+++ SetecAstronomyAzureKeyVault/ConfigProvider/CustomConfigurationProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConfigProvider
+{
+    class CustomConfigurationProvider:ProtectedConfigurationProvider
+    {
+        public override void Initialize(string name, NameValueCollection config)
+        {
+            base.Initialize(name, config);
+        }
+
+        public override XmlNode Decrypt(XmlNode encryptedNode)
+        {
+            // We'll flesh this out below.
+            throw new NotImplementedException();
+        }
+
+        public override XmlNode Encrypt(XmlNode node)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SetecAstronomyAzureKeyVault/PowerShell Scripts/Create-Key-Vault.ps1
@@ -0,0 +1,25 @@
+﻿$env:PSModulePath += ";C:\Program Files (x86)\Microsoft SDKs\Azure\PowerShell\ResourceManager\AzureResourceManager"
+Import-Module "C:\Program Files (x86)\Microsoft SDKs\Azure\PowerShell\ResourceManager\AzureResourceManager\AzureResourceManager.psd1" # <-- Now you can do this!
+
+Import-AzurePublishSettingsFile "C:\azure\azure-credentials.publishsettings"
+
+Write-Host 'Check For Vault'
+Get-AzureKeyVault -VaultName 'Setec'
+
+Write-Host 'Remove Vault'
+Remove-AzureKeyVault -VaultName 'Setec' -Force
+
+Write-Host 'Add Resource Group'
+New-AzureResourceGroup –Name 'SetecAstronomy' –Location 'East US' -Force
+
+Write-Host 'Add Vault'
+New-AzureKeyVault -VaultName 'Setec' -ResourceGroupName 'SetecAstronomy' -Location 'East US'
+
+Write-Host 'Add FirstSecret'
+$secretvalue = ConvertTo-SecureString -String "I leave message here on service but you do not call" -AsPlainText -Force
+$secret = Set-AzureKeyVaultSecret -VaultName "Setec" -Name "FirstSecret" -SecretValue $secretvalue
+
+Write-Host "FirstSecret ID: " + $secret.Id
+
+$AzureADApplicationKey = 'Insert Azure AD Application Key Here'
+Set-AzureKeyVaultAccessPolicy -VaultName 'Setec' -ServicePrincipalName $AzureADApplicationKey -PermissionsToSecrets all -PermissionsToKeys all
